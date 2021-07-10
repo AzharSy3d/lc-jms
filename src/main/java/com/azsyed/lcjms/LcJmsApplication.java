031e7f88ef61a1f003affe36846c2f1e17c96111
AzharSy3d <azharuworld@gmail.com>
2021-07-10 18:08:10 +0530

    Setup Initial JMS project

---
+++ src/main/java/com/azsyed/lcjms/LcJmsApplication.java
@@ -0,0 +1,13 @@
+package com.azsyed.lcjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/azsyed/lcjms/LcJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azsyed.lcjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
